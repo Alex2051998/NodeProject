c3e0c858c9d2c723ffaec9d4ee54c21312dce910
Alex20511998 <Alex-2504@ukr.net>
2022-03-07 11:10:29 +0200

    node.js_lesson5

---
+++ lesson5/dist/app.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const typeorm_1 = require("typeorm");
+const user_1 = require("./entity/user");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded());
+app.get('/users', async (req, res) => {
+    const users = await (0, typeorm_1.getManager)().getRepository(user_1.User).find();
+    console.log(users);
+    res.json(users);
+});
+app.post('/users', async (req, res) => {
+    console.log(req.body);
+    const createdUser = await (0, typeorm_1.getManager)().getRepository(user_1.User).save(req.body);
+    res.json(createdUser);
+});
+app.listen(5200, async () => {
+    console.log('Server has started!!!!');
+    try {
+        const connection = await (0, typeorm_1.createConnection)();
+        if (connection) {
+            console.log('Database connected');
+        }
+    }
+    catch (err) {
+        if (err)
+            console.log(err);
+    }
+});
+//# sourceMappingURL=app.js.map
